c2c9aa55ebad638f0f5271b1d5a0fb16b070c1be
yuanchingchen <yuanching.chen.appledev@gmail.com>
2020-03-05 18:54:32 -0600

    Update the Altitude and Ground Speed calculation with get_max_value_by_type()

--- solution.py
+++ solution.py
@@ -4,25 +4,31 @@ NetId: ycchen4
 """
 
 import pandas as pd
+import math
 from datetime import datetime
 from geographiclib.geodesic import Geodesic
 
 
 def analyze_aircraft_data(file_name: str, update_interval=5):
+    """
+    The main function to process the raw data line by line
+    Output the data each update_interval
+    :param file_name: The file name of raw aircraft data
+    :param update_interval: The int value represent how many minutes the user want to output the analysis result
+    :return: None
+    """
     if file_name is "" or file_name is None:
         raise ValueError("file does not exist")
 
     with open(file_name, 'r') as input_file:
         start_time = None
         end_time = None
-        time_gap = None
         data_in_interval = []
         current_result_col_names = ['TIME', '# Craft', 'Fastest (kts)', 'Highest (ft)', 'Msgs/Sec']
-        current_result_df = pd.DataFrame(columns=current_result_col_names)
         cumulative_result_df = pd.DataFrame(columns=['#Craft', 'LongestTrack', '(nm)'])
 
         count = 0
-        # read line by line
+
         for line in input_file:
             current_line_list = line.split(',')
 
@@ -32,59 +38,93 @@ def analyze_aircraft_data(file_name: str, update_interval=5):
             end_time = current_time
             if start_time is None:
                 start_time = current_time
-                print(start_time)
 
             cur_data_list = [current_line_list[i] for i in [8, 9, 4, 11, 12, 14, 15]]
             data_in_interval.append(cur_data_list)
 
             time_interval = (end_time - start_time).total_seconds()
             if time_interval >= update_interval * 60:
-                print(end_time)
                 """ CURRENT RESULTS """
-
                 time_col = end_time.strftime("%m/%d %H:%M:%S")
-                # reset start and end time
+
+                # -------- reset start and end time --------
                 start_time = end_time
                 end_time = None
 
-                # init the pandas data frame
+                # -------- init the current result data frame --------
                 col_names = ['DateLogged', 'TimeLogged', 'AircraftHex', 'Altitude', 'GroundSpeed', 'Latitude', 'Longitude']
-                data_frame = pd.DataFrame(data=data_in_interval, columns=col_names)
+                current_result_df = pd.DataFrame(data=data_in_interval, columns=col_names)
 
-                # craft & Fastest
-                craft_no_col = len(data_frame.groupby('AircraftHex').agg(['count']))
+                # reset the data frame
+                data_in_interval = []
 
-                # fastest
-                fastest_col = get_max_by('GroundSpeed', data_frame.groupby(['AircraftHex'])['GroundSpeed'].max().reset_index())
+                # -------- number of aircraft --------
+                craft_no_col = len(current_result_df.groupby('AircraftHex').agg(['count']))
 
-                # Highest
-                highest_col = get_max_by('Altitude', data_frame.groupby(['AircraftHex'])['Altitude'].max().reset_index())
+                # -------- fastest --------
+                temp_df = current_result_df[['AircraftHex', 'GroundSpeed']]
+                temp_df = temp_df[temp_df['GroundSpeed'] != '']
+                fastest_col = get_max_value_by_type('GroundSpeed', temp_df)
 
-                # Msgs / Sec
-                msg_per_sec_col = round(data_frame.shape[0] / time_interval, 1)
+                # -------- Highest --------
+                temp_df = current_result_df[['AircraftHex', 'Altitude']]
+                temp_df = temp_df[temp_df['Altitude'] != '']
+                highest_col = get_max_value_by_type('Altitude', temp_df)
+
+                # -------- Msgs / Sec --------
+                msg_per_sec_col = round(current_result_df.shape[0] / time_interval, 1)
 
                 new_df = pd.DataFrame(columns=current_result_col_names)
                 new_df.loc[0] = [time_col, craft_no_col, fastest_col, highest_col, msg_per_sec_col]
-                current_result_df = pd.concat([current_result_df, new_df])
+                # current_result_df = pd.concat([current_result_df, new_df])
 
-                print(current_result_df)
-                print('----------------------------------')
+                if count == 0:
+                    print("***************** CURRENT RESULTS ******************* | ******* CUMULATIVE RESULTS *******")
+                    print('TIME # Craft  Fastest  (kts)  Highest  (ft)  Msgs/Sec')
 
-                """ CUMULATIVE RESULTS """
-                data_frame.groupby(['AircraftHex']).apply(get_distance)
+                print(time_col, craft_no_col, fastest_col, highest_col, msg_per_sec_col)
+
+                """CUMULATIVE RESULTS"""
+                temp_df = current_result_df[['AircraftHex', 'Latitude', 'Longitude']]
+                temp_df['Latitude'] = temp_df['Latitude'].replace('', 0.0).astype(float)
+                temp_df['Longitude'] = temp_df['Longitude'].replace('', 0.0).astype(float)
+                temp_df = temp_df[(current_result_df['Latitude'] != 0.0) & (current_result_df['Longitude'] != 0.0)]
+                distance_sum = temp_df.groupby(['AircraftHex']).apply(shift_sum, axis='columns').reset_index()
+                return
+
                 # cumulative_result_df = pd.concat([cumulative_result_df, current_distance_df])
-
-                if count > 0:
-                    break
-
-                count += 1
+                # print(cumulative_result_df)
 
 
-def get_max_by(type: str, df: pd.DataFrame) -> str:
-    max_speed = df[type].max()
-    max_speed_craft = df[df[type] == max_speed]
+def shift_sum(df: pd.DataFrame):
+    print(df)
+    return
+    # 新增兩籃 => shift axis = 0, periods = 1
+    df[['last_lat', 'last_lon']] = df[['Latitude', 'Longitude']] = df[['Latitude', 'Longitude']].shift(axis=0, periods=1)
+    new_df = df[['last_lat', 'last_lon', 'Latitude', 'Longitude']].iloc[1, :].reset_index(drop=True)
 
-    return str(max_speed_craft.iloc[0]['AircraftHex']) + ' ' + str(max_speed_craft.iloc[0][type])
+    new_df.apply(calculate_the_distance, axis=1)
+
+
+def calculate_the_distance(last_latitude: float, last_longitude: float, current_latitude: float, current_longitude: float) -> float:
+    geod = Geodesic.WGS84
+    return round(geod.Inverse(last_latitude, last_longitude, current_latitude, current_longitude)['s12'] / 1852.0, 6)
+
+
+def get_max_value_by_type(column_name: str, df: pd.DataFrame) -> str:
+    """
+    Get the maximum value of Altitude or Ground Speed
+    :param column_name: string type, 'Altitude' or 'Groundspeed'
+    :param df: the target data frame
+    :return: the output string
+    """
+    df[column_name] = df[column_name].replace('', 0).astype(float)
+    group_df = df.groupby(['AircraftHex'])[column_name].max().reset_index()
+    group_df = group_df.sort_values(column_name, ascending=False)
+
+    # get the max value from row 0, col all
+    max_value = group_df.iloc[0, :]
+    return str(max_value[0]) + ' ' + str(math.trunc(max_value[1]))
 
 
 def get_datetime_from_string(date_string: str) -> datetime:
@@ -106,38 +146,6 @@ def get_datetime_from_string(date_string: str) -> datetime:
     date_format = "%Y/%m/%d %H:%M:%S.%f"
     datetime_obj = datetime.strptime(date_string, date_format)
     return datetime_obj
-
-
-def get_distance(df: pd.DataFrame) -> pd.DataFrame:
-    df = df[(df['Latitude'] != '') & (df['Longitude'] != '')]
-    if df.empty:
-        return
-
-    prev_lat = None
-    prev_lon = None
-    distance_list = []
-    for index, row in df.iterrows():
-        if prev_lat is None and prev_lon is None:
-            prev_lat = float(row['Latitude'])
-            prev_lon = float(row['Longitude'])
-            distance_list.append(0.0)
-            continue
-
-        current_lat = float(row['Latitude'])
-        current_long = float(row['Longitude'])
-        distance = calculate_the_distance(prev_lat, prev_lon, current_lat, current_long)
-        prev_lat = current_lat
-        prev_lon = current_long
-        distance_list.append(distance)
-
-    df['Distance'] = distance_list
-    # print(df.groupby(['AircraftHex'])['Distance'].sum())
-    return df.groupby(['AircraftHex'])['Distance'].sum().reset_index()
-
-
-def calculate_the_distance(last_latitude: float, last_longitude: float, current_latitude: float, current_longitude: float) -> float:
-    geod = Geodesic.WGS84
-    return round(geod.Inverse(last_latitude, last_longitude, current_latitude, current_longitude)['s12'] / 1852.0, 8)
 
 
 if __name__ == '__main__':
